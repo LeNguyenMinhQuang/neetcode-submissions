a0785d4ffa715b0cc6e5a50d4a61fd7fc0745760
Quang <99551189+LeNguyenMinhQuang@users.noreply.github.com>
2026-09-08 09:03:40 +0700

    Add: three-integer-sum - submission-6

---
+++ Data Structures & Algorithms/three-integer-sum/submission-6.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        nums = nums.sort((a,b) => a-b)
+        const n = nums.length;
+        let res = [];
+        for (let i = 0; i < n; i++){
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] === nums[i-1]) continue;
+            let j = i + 1, k = n - 1;
+            while (j < k){
+                let sum = nums[i] + nums[j] + nums[k];
+                if (sum === 0) {
+                    res.push([nums[i], nums[j], nums[k]]);
+                    j++; k--;
+                    while (j < k && nums[j] == nums[j-1]) j++
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    j++;
+                }
+                 
+            }
+        }
+        return res
+    }
+}
