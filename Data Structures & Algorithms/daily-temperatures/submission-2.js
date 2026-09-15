77f58dc868076e49aca55f2dea6fc100301a6252
Quang <99551189+LeNguyenMinhQuang@users.noreply.github.com>
2026-09-15 10:32:07 +0700

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        let n = temperatures.length;
+        let res = Array.from({length: n}, () => 0);
+        let stack = [];
+        for (let i = 0; i < n; i++){
+            while (stack.length > 0 && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+                let prev = stack.pop();
+                res[prev] = i - prev;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+}
