2695cb14da4f2f55bba3a1f8df42659159583d7c
Quang <99551189+LeNguyenMinhQuang@users.noreply.github.com>
2026-09-18 09:48:55 +0700

    Add: car-fleet - submission-5

---
+++ Data Structures & Algorithms/car-fleet/submission-5.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target, position, speed) {
+        let stack = [];
+        let car = [];
+        let n = position.length;
+        for (let i = 0; i < n; i++){
+            car.push([position[i], speed[i]]);
+        }
+        car.sort(([a,b],[c,d]) => c-a);
+        for (let [p,s] of car){
+            let time = (target - p)/s;
+            if (stack.length == 0){
+                stack.push(time);
+            } else {
+                if (time > stack[stack.length -1]){
+                    stack.push(time);
+                }
+            }
+        }
+        return stack.length;
+    }
+}
