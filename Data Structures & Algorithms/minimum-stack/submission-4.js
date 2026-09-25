2a0a7ba191605bf6ce61acd52b06bea46a6fb523
Quang <99551189+LeNguyenMinhQuang@users.noreply.github.com>
2026-09-25 14:15:48 +0700

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.js
@@ -0,0 +1,42 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.min = []
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+       this.stack.push(val);
+
+        if (this.min.length === 0) {
+            this.min.push(val);
+        } else {
+            this.min.push(Math.min(val, this.getMin()));
+        }
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+       this.stack.pop();
+       this.min.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length - 1];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.min[this.min.length - 1];
+    }
+}
