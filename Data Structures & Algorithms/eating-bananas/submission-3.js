a6abe8cc3822cb7be614706eedb2905bb67e956c
Quang <99551189+LeNguyenMinhQuang@users.noreply.github.com>
2026-09-25 13:39:12 +0700

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+        let left = 1;
+        let right = Math.max(...piles);
+        let res = right;
+        while (left <= right){
+            let mid = Math.floor((left+right)/2);
+            let ht = 0;
+            for (let p of piles){
+                ht += Math.ceil(p/mid);
+            }
+            if (ht <= h) {
+                res = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return res;
+    }
+}
